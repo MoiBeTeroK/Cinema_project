29ce883c48bdb8ca35a2ae50059ff953b0681d38
MoiBeTeroK <ludarikkot@gmail.com>
2025-01-14 21:19:09 +0300

    feat: add movie deletion

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,6 +24,12 @@ class MoviesController < ApplicationController
     end
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path, notice: 'Фильм был успешно удален.'
+  end
+
   private
 
   def movie_params
